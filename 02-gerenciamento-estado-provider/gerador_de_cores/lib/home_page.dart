416429e71184e6b4a4edd4581a575a57b70e24c5
Luisa <luisa@Last-Mac-Kobe.local>
2025-06-30 19:49:39 -0300

    feat: Finaliza app gerador de cores com Provider

--- 02-gerenciamento-estado-provider/gerador_de_cores/lib/home_page.dart
+++ 02-gerenciamento-estado-provider/gerador_de_cores/lib/home_page.dart
@@ -1,13 +1,49 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:gerador_de_cores/color_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Gerador de cores')),
-      body: Column(children: [Text(data)]),
+    return Consumer<ColorProvider>(
+      builder: (context, cor, _) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Gerador de cores')),
+          backgroundColor: cor.corAtual,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'RGB: ${cor.corAtual.red}, ${cor.corAtual.green}, ${cor.corAtual.blue}',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 7.0,
+                        color: Colors.black.withOpacity(0.5),
+                        offset: Offset(2.0, 2.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Provider.of<ColorProvider>(context, listen: false).gerarNovaCor();
+            },
+            child: Icon(Icons.change_circle),
+          ),
+        );
+      },
     );
   }
 }
