b688524ca15a2e416ffba010dedbb7a2717bb35b
Luisa <luisa@Last-Mac-Kobe.local>
2025-07-03 12:00:34 -0300

    feat: adiciona funcionalidades de criar novas tarefas via dialog

--- 03-app-lista-de-tarefas/to_do_app/lib/pages/home_page.dart
+++ 03-app-lista-de-tarefas/to_do_app/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_app/provider/tarefa_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,10 +10,71 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha lista de tarefas'),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.purple[200],
         foregroundColor: Colors.white,
       ),
-      body: Center(child: Text('Minhas tarefas vão aparecer aqui')),
+      body: Consumer<TarefaProvider>(
+        builder: (context, tarefa, _) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(
+                  tarefa.tarefas[index].titulo,
+                  style: TextStyle(color: Colors.purple[800], fontSize: 20),
+                ),
+                leading: Checkbox(
+                  value: tarefa.tarefas[index].isDone,
+                  onChanged: (value) {},
+                ),
+              );
+            },
+            itemCount: tarefa.tarefas.length,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _mostrarDialogoDeTarefa(context);
+        },
+        backgroundColor: Colors.purple[200],
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+      ),
     );
   }
+}
+
+void _mostrarDialogoDeTarefa(BuildContext context) {
+  final TextEditingController text = TextEditingController();
+
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Adicionar nova tarefa'),
+        content: TextField(controller: text),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancelar'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (text.text.isNotEmpty) {
+                Provider.of<TarefaProvider>(
+                  context,
+                  listen: false,
+                ).criarTarefa(text.text);
+
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Adicionar'),
+          ),
+        ],
+      );
+    },
+  );
 }
