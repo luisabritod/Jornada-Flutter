c384a4c69677915c6c2ff682639816988b4eac70
Luisa <luisa@MacBookPro.home>
2025-07-05 15:43:43 -0300

    feat: exibe lista de produtos da API na tela

--- 04-consumindo-apis/vitrine_app/lib/pages/home_page.dart
+++ 04-consumindo-apis/vitrine_app/lib/pages/home_page.dart
@@ -20,6 +20,59 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Vitrine')),
+      body: Center(
+        child: Consumer<ProdutoProvider>(
+          builder: (context, produtosProvider, _) {
+            if (produtosProvider.carregando) {
+              return CircularProgressIndicator();
+            } else {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final produto = produtosProvider.produtos[index];
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          produto.image,
+                          height: 120,
+                          width: double.infinity,
+                          fit: BoxFit.contain,
+                        ),
+                        Padding(
+                          padding: EdgeInsetsGeometry.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                produto.title,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                'R\$: ${produto.price.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  color: Colors.green[700],
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: produtosProvider.produtos.length,
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
