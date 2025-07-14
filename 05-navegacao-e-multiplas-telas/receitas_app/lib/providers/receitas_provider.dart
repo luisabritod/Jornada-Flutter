869298605a3324db4d9fc15bc8462d72c70c17b2
Luisa <luisa@Last-Mac-Kobe.local>
2025-07-14 20:04:21 -0300

    feat(receitas): implementa lógica para buscar detalhes da receita no provider

--- 05-navegacao-e-multiplas-telas/receitas_app/lib/providers/receitas_provider.dart
+++ 05-navegacao-e-multiplas-telas/receitas_app/lib/providers/receitas_provider.dart
@@ -6,13 +6,15 @@ import 'package:http/http.dart' as http;
 
 class ReceitasProvider extends ChangeNotifier {
   List<ReceitaResumo> _receitas = [];
-  bool _isDone = false;
+  bool _carregando = false;
+  ReceitaDetalhada? _receitaDetalhada;
 
   List<ReceitaResumo> get receitas => _receitas;
-  bool get carregando => _isDone;
+  bool get carregando => _carregando;
+  ReceitaDetalhada? get receitaDetalhada => _receitaDetalhada;
 
   void buscarReceitas() async {
-    _isDone = true;
+    _carregando = true;
     notifyListeners();
 
     try {
@@ -32,7 +34,31 @@ class ReceitasProvider extends ChangeNotifier {
     } catch (e) {
       print(e);
     } finally {
-      _isDone = false;
+      _carregando = false;
+      notifyListeners();
+    }
+  }
+
+  void buscarDetalhesDaReceita(String id) async {
+    _carregando = true;
+    notifyListeners();
+
+    try {
+      final response = await http.get(
+        Uri.parse("https://www.themealdb.com/api/json/v1/1/lookup.php?i=$id"),
+      );
+
+      final json = jsonDecode(response.body);
+
+      if (json["meals"] != null) {
+        final Map<String, dynamic> dadosDaReceita = json["meals"][0];
+
+        _receitaDetalhada = ReceitaDetalhada.fromJson(dadosDaReceita);
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      _carregando = false;
       notifyListeners();
     }
   }
