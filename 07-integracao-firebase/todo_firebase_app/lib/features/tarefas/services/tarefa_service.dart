157d4ced1b874bea8cb1185513b9daea7673278d
Luisa <luisa@Last-Mac-Kobe.local>
2025-07-23 12:45:43 -0300

    feat(todo-firebase): implementa Service para operações de leitura e criação no Firestore

---
+++ 07-integracao-firebase/todo_firebase_app/lib/features/tarefas/services/tarefa_service.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TarefaService {
+  // 1. criamos uma referência para a "coleção de tarefas Firestore.
+  // pense nisso como o "endereço" da nossa gaveta de tarefas na nuvem.
+  // se a coleção 'tarefas' não existir, o Firebase a cria.
+  final CollectionReference _tarefasCollection = FirebaseFirestore.instance
+      .collection('tarefas');
+
+  // os métodos para criar, ler, atualizar e deletar (CRUD) virão aqui
+
+  // --- MÉTODO PARA LER AS TAREFAS (READ) ---
+  // esse método é especial: ele retorna um "cano" (Stream) de dados
+  // toda vez que algo mudar na coleção 'tarefas' no Firebase, a informação nova descerá por esse cano automaticamente e em tempo real
+  Stream<QuerySnapshot> getTarefaStream() {
+    // .snapshot() nos dá esse "cano" em tempo real da nossa coleção
+    return _tarefasCollection.snapshots();
+  }
+
+  // --- MÉTODO PARA ADICIONAR UMA TAREFA (CREATE) ---
+  // esse método será chamado quando o usuário digitar uma nova tarefa
+  Future<void> adicionarTarefa(String titulo) {
+    // o Firestore espera um Map, exatamente como esta no método toFirestore() na classe Tarefa
+    // usamos .add() e o Firebase cuida de criar o documentos com ID único
+    return _tarefasCollection.add({
+      'titulo': titulo,
+      'concluida': false,
+      'timestamp':
+          FieldValue.serverTimestamp(), // boa prática: salva a data de criação
+    });
+  }
+}
