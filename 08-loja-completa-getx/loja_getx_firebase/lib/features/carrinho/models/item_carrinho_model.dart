0a66f8d02402153cd5fa56007ebed969d1fff29d
Luisa <luisa@Last-Mac-Kobe.local>
2025-07-30 10:23:20 -0300

    feat: define modelos de dados iniciais

---
+++ 08-loja-completa-getx/loja_getx_firebase/lib/features/carrinho/models/item_carrinho_model.dart
@@ -0,0 +1,28 @@
+import 'package:loja_getx_firebase/features/produtos/models/produto_model.dart';
+
+class ItemCarrinho {
+  final String id;
+  final String title;
+  final double price;
+  final String image;
+  final int quantity;
+
+  ItemCarrinho({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
+    required this.quantity,
+  });
+
+  // constrói um ItemCarrinho a partir de um produto
+  factory ItemCarrinho.fromProduto(Produto produto) {
+    return ItemCarrinho(
+      id: produto.id.toString(),
+      title: produto.title,
+      price: produto.price,
+      image: produto.image,
+      quantity: 1,
+    );
+  }
+}
