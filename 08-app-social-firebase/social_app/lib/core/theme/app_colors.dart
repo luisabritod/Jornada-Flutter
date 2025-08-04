8ff6edb8f0d5b78e31c9ff2082ce631f2187c43a
Luisa <luisa@Last-Mac-Kobe.local>
2025-08-04 14:08:30 -0300

    feat(social_app): adiciona paleta de cores ao projeto

---
+++ 08-app-social-firebase/social_app/lib/core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // paleta principal
+  static const Color primary = Color(0xFFAE92C9); //lílas
+  static const Color accent = Color(0xFF7Ea4bb); //azul acinzentado
+
+  // cores neutras
+  static const Color background = Color(0xFFFAF9FB); // branco levemente lilás
+  static const Color surface = Colors.white;
+  static const Color text = Color(0xFF3D3B40); // cinza escuro
+  static const Color textLight = Colors.white;
+
+  // cores de feedback
+  static const Color success = Color(0xFF2E7D32);
+  static const Color error = Color(0xFFD32F2F);
+}
