ff42f050bf698c15e573b890604143e91f6e7a13
Luisa <luisa@Last-Mac-Kobe.local>
2025-09-11 15:39:51 -0300

    exclui social app

--- 08-app-social-firebase/social_app/android/app/src/main/kotlin/br/com/luisa/dev/social_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package br.com.luisa.dev.social_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- 08-app-social-firebase/social_app/lib/core/constants/ui_constants.dart
+++
@@ -1,5 +0,0 @@
-class UIConstants {
-  static const double defaultPadding = 16.0;
-  static const double defaultRadius = 8.0;
-  static const double defaultSpacing = 8.0;
-}
